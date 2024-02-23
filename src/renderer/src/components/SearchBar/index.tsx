26fc619786261722028ec38fb6b0a39f539f47e9
Amandalfs <amandalfs57@gmail.com>
2024-02-23 13:57:31 -0300

    feat: create search by alias

--- src/renderer/src/components/SearchBar/index.tsx
+++ src/renderer/src/components/SearchBar/index.tsx
@@ -1,11 +1,46 @@
+import { useQuery } from '@tanstack/react-query'
 import { Command } from 'cmdk'
 import { File, MagnifyingGlass } from 'phosphor-react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export const SearchBar = (): JSX.Element => {
+interface SearchBarProps {
+  isOpen: boolean
+  onChangeOpen: (isOpen: boolean) => void
+}
+
+export const SearchBar = ({ isOpen, onChangeOpen }: SearchBarProps): JSX.Element => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    const down = (event: KeyboardEvent): void => {
+      if (event.key === 'k' && event.ctrlKey) {
+        onChangeOpen(!isOpen)
+      }
+    }
+
+    document.addEventListener('keydown', down)
+    return () => document.removeEventListener('keydown', down)
+  }, [onChangeOpen, isOpen])
+
+  const { data: documents } = useQuery({
+    queryKey: ['documents'],
+    queryFn: async () => {
+      const response = await window.api.fetchDocuments()
+      return response.data
+    }
+  })
+
+  function handleOpenDocument(id: string): void {
+    onChangeOpen(false)
+    navigate(`/document/${id}`)
+  }
+
   return (
     <Command.Dialog
       className="fixed top-24 left-1/2 -translate-x-1/2 w-[480px] max-w-full bg-rotion-800 rounded-md shadow-2xl text-rotion-100 border border-rotion-600"
       label="Search"
+      open={isOpen}
+      onOpenChange={onChangeOpen}
     >
       <div className="flex items-center gap-2 border-b border-rotion-700 p-4">
         <MagnifyingGlass className="w-5 h-5" />
@@ -15,15 +50,22 @@ export const SearchBar = (): JSX.Element => {
           className="w-full bg-transparent focus:outline-none text-sm text-rotion-50 placeholder:text-rotion-200"
         />
       </div>
-      <Command.List className="py-2 max-h-48 scrollbar-thin scrollbar-thumb-rotion-600 scrollbar-track-rotion-800">
+      <Command.List className="py-2 max-h-48 scrollbar-thin scrollbar-thumb-rotion-600 scrollbar-track-rotion-800 overflow-y-auto">
         <Command.Empty className="py-3 px-4 text-rotion-200 text-sm">
           No page was found.
         </Command.Empty>
 
-        <Command.Item className="py-3 px-4 text-rotion-50 text-sm flex items-center gap-2 hover:bg-rotion-700 aria-selected:!bg-rotion-600">
-          <File className="w-4 h-4" />
-          Titulo
-        </Command.Item>
+        {documents &&
+          documents.map((documentList) => (
+            <Command.Item
+              key={documentList.id}
+              onSelect={() => handleOpenDocument(documentList.id)}
+              className="py-3 px-4 text-rotion-50 text-sm flex items-center gap-2 hover:bg-rotion-700 aria-selected:!bg-rotion-600"
+            >
+              <File className="w-4 h-4" />
+              {documentList.title}
+            </Command.Item>
+          ))}
       </Command.List>
     </Command.Dialog>
   )
--- src/renderer/src/components/Sidebar/Search.tsx
+++ src/renderer/src/components/Sidebar/Search.tsx
@@ -1,8 +1,14 @@
 import { MagnifyingGlass } from 'phosphor-react'
 
 import { SearchBar } from '../SearchBar'
+import { useState } from 'react'
 
 export const Search = (): JSX.Element => {
+  const [isSearchBarOpen, setIsSearchBarOpen] = useState<boolean>(false)
+
+  function toggleSearchBar(isOpen: boolean): void {
+    setIsSearchBarOpen(isOpen)
+  }
   return (
     <>
       <button className="flex mx-5 items-center gap-2 text-rotion-100 text-sm hover:text-rotion-50">
@@ -10,7 +16,7 @@ export const Search = (): JSX.Element => {
         Search...
       </button>
 
-      <SearchBar />
+      <SearchBar isOpen={isSearchBarOpen} onChangeOpen={toggleSearchBar} />
     </>
   )
 }
